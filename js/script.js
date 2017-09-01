7dd56c06d8df375159c68ee9d94a87c5f12f6f5c
sorre97 <Soldier@macusers-MacBook-Pro.local>
2017-09-02 01:01:17 +0200

    More changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function main(){
+    /*
+    
     $(".title").hide().fadeIn(1000).delay(3500).fadeOut(1000);
-
     $(".caption").hide().delay(500).fadeIn(1000).delay(2500).fadeOut(1000);
     $(".overlay").delay(6000).fadeOut(200)
     $(".screen").delay(6000).animate({
         left: "+=100%",
     },500);
+    */
+    
+    $(".debug").hide();
+    
 }
 
 $(document).ready(main());
