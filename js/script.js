8cd14eb14ebc88555e5333ca45865b59cafb406e
sorre97 <Soldier@macusers-MacBook-Pro.local>
2017-09-02 09:32:11 +0200

    Front-end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var form1 = undefined;
+var form2 = undefined;
+
 function main(){
     /*
     
@@ -10,6 +13,26 @@ function main(){
     */
     
     $(".debug").hide();
+    
+    $(".decBut").on('click',function(){
+        form1 = true;
+        if(form2 == true){
+            form2 = false;
+           $(".binToDec").fadeTo(100,0);
+        }
+        $(".binToDec").removeClass("priority");
+        $(".decToBin").addClass("priority").fadeTo(400,1);
+    })
+    
+    $(".binBut").on('click',function(){
+        form2 = true;
+        if(form1 == true){
+            form1 = false;
+           $(".decToBin").fadeTo(100,0);
+        }
+        $(".decToBin").removeClass("priority");
+        $(".binToDec").addClass("priority").fadeTo(400,1);
+    })
     
 }
 
